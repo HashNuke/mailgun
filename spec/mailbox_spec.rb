c7e01fb101c49262cc597e23b49f042ed7042e3c
Akash Manohar J <akash@akash.im>
2011-11-19 00:30:11 +0530

    added sample response to fix mailboxes.list test; specs green
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/mailbox_spec.rb
+++ spec/mailbox_spec.rb
@@ -2,56 +2,56 @@ require 'spec_helper'
 
 describe Mailgun::Mailbox do
 
-	before :each do
-		@mailgun = Mailgun({:api_key => "api-key"})		# used to get the default values
+  before :each do
+    @mailgun = Mailgun({:api_key => "api-key"})		# used to get the default values
 
-		@mailbox_options = {
-      :email  => "test@sample.mailgun.com",
+    @mailbox_options = {
+      :email  => "test@sample.mailgun.org",
       :name   => "test",
-      :domain => "sample.mailgun.com"
+      :domain => "sample.mailgun.org"
     }
-	end
+  end
 
-	describe "list_mailboxes" do
-		it "should make a get request with the right params" do
-			RestClient.should_receive(:get)
-      .with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes", {}).and_return("{}")
+  describe "list mailboxes" do
+    it "should make a get request with the right params" do
+      sample_response = "{\n  \"total_count\": 1,\n  \"items\": [\n  {\n  \"size_bytes\": 0,\n  \"mailbox\": \"postmaster@bsample.mailgun.com\"\n    }\n  ]\n}"
+      RestClient.should_receive(:get).with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes", {}).and_return(sample_response)
 
-			@mailgun.mailboxes.list @mailbox_options[:domain]
-		end
-	end
+      @mailgun.mailboxes.list @mailbox_options[:domain]
+    end
+  end
 
-	describe "create mailbox" do
-		it "should make a post request with the right params"	do
-			RestClient.should_receive(:post)
-								.with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes",
-											:mailbox  => @mailbox_options[:email],
-											:password => @mailbox_options[:password])
-								.and_return({})
+  describe "create mailbox" do
+    it "should make a post request with the right params"	do
+      RestClient.should_receive(:post)
+        .with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes",
+        :mailbox  => @mailbox_options[:email],
+        :password => @mailbox_options[:password])
+        .and_return({})
 
-			@mailgun.mailboxes.create(@mailbox_options[:email], @mailbox_options[:password])
-		end
-	end
+      @mailgun.mailboxes.create(@mailbox_options[:email], @mailbox_options[:password])
+    end
+  end
 
-	describe "update mailbox password" do
-		it "should make a put request with the right params" do
-			RestClient.should_receive(:put)
-								.with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes/#{@mailbox_options[:name]}",
-											:password => @mailbox_options[:password])
-								.and_return({})
+  describe "update mailbox password" do
+    it "should make a put request with the right params" do
+      RestClient.should_receive(:put)
+        .with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes/#{@mailbox_options[:name]}",
+        :password => @mailbox_options[:password])
+        .and_return({})
 
-			@mailgun.mailboxes.update_password @mailbox_options[:email],
-                                         @mailbox_options[:password]
-		end
-	end
+      @mailgun.mailboxes.update_password @mailbox_options[:email],
+      @mailbox_options[:password]
+    end
+  end
 
-  	describe "destroy mailbox" do
-		it "should make a put request with the right params" do
-			RestClient.should_receive(:delete)
-      .with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes/#{@mailbox_options[:name]}", {})
-      .and_return({})
+  describe "destroy mailbox" do
+    it "should make a put request with the right params" do
+      RestClient.should_receive(:delete)
+        .with("#{@mailgun.send(:base_url)}/#{@mailbox_options[:domain]}/mailboxes/#{@mailbox_options[:name]}", {})
+        .and_return({})
 
-			@mailgun.mailboxes.destroy @mailbox_options[:email]
-		end
-	end
+      @mailgun.mailboxes.destroy @mailbox_options[:email]
+    end
+  end
 end
