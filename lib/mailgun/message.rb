5f6c62ee98edb6319f158028a005257951349d9a
Alan deLevie <adelevie@gmail.com>
2013-02-26 12:13:03 -0500

    added Mailgun::Message#send_email

--- lib/mailgun/message.rb
+++ lib/mailgun/message.rb
@@ -1,13 +1,13 @@
 module Mailgun
-  class Mail
-
-    def initialize(mailgun)
+  class Message
+    def initialize(mailgun, domain)
       @mailgun = mailgun
+      @domain  = domain
     end
 
     # send email
-    def send_email()
-      # TODO with the following options
+    def send_email(parameters={})
+      # options:
       # :from, :to, :cc, :bcc, :subject, :text, :html 
       # :with_attachment
       # :with_attachments
@@ -15,6 +15,14 @@ module Mailgun
       # :in_test_mode BOOL. override the @use_test_mode setting
       # :tags to add tags to the email
       # :track BOOL
+      Mailgun.submit(:post, messages_url, parameters)
+    end
+
+    #private
+
+    # Helper method to generate the proper url for Mailgun complaints API calls
+    def messages_url
+      "#{@mailgun.base_url}/#{@domain}/messages"
     end
   end
 end
