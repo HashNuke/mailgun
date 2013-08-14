390c6e8dc3ebec90429dc364d2c13ce760d98a74
Andrés Bravo <andresbravog@gmail.com>
2013-08-14 11:22:12 +0200

    Added destroy to bounces and some doc

--- lib/mailgun/bounce.rb
+++ lib/mailgun/bounce.rb
@@ -10,17 +10,38 @@ module Mailgun
     end
     
     # List all bounces for a given domain
+    #
+    # @param options [Hash] options to populate to mailgun find
+    # @option options limit (100) [Integer] limit of results
+    # @option options skip  (0)   [Integer] number of results to skip
+    #
+    # @returns [Array<Hash>] array of bouces
     def list(options={})
       Mailgun.submit(:get, bounce_url, options)["items"] || []
     end
 
     # Find bounce events for an email address
+    #
+    # @returns [<Hash>] found bouce
     def find(email)
       Mailgun.submit :get, bounce_url(email)
     end
 
-    def add(email)
-      Mailgun.submit :post, bounce_url, :address => email
+    # Creates a bounce for an email address
+    #
+    # @param email [String] email address to bounce
+    # @param options [Hash] options to populate to mailgun bounce creation
+    # @option options code (550) [Integer] Error code
+    # @option options error  ('')   [String] Error description
+    #
+    # @returns [<Hash>] created bouce
+    def add(email, options={})
+      Mailgun.submit :post, bounce_url, {:address => email}.merge(options)
+    end
+    
+    # Cleans the bounces for an email address
+    def destroy(email)
+      Mailgun.submit :delete, bounce_url(email)  
     end
 
     private
