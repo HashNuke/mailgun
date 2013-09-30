458b9f9442df03aab8fd1960c7b93240dad11e3f
Alan Sikora <alansikora@gmail.com>
2013-09-30 16:08:20 -0300

    adding secure module to check for webhooks authenticity

--- lib/mailgun.rb
+++ lib/mailgun.rb
@@ -14,6 +14,7 @@ require "mailgun/log"
 require "mailgun/list"
 require "mailgun/list/member"
 require "mailgun/message"
+require "mailgun/secure"
 
 #require "startup"
 
--- lib/mailgun/base.rb
+++ lib/mailgun/base.rb
@@ -61,6 +61,10 @@ module Mailgun
     def list_members(address)
       Mailgun::MailingList::Member.new(self, address)
     end
+    
+    def secure
+      Mailgun::Secure.new(self)
+    end
   end
 
 
